c5d3c49a25773f9d4acf32dc172f0fac2d407ecb
Kesha <98253713+keshah239@users.noreply.github.com>
2025-04-07 17:50:56 +0300

    Add files via upload

---
+++ DLL+DML.sql
@@ -0,0 +1,130 @@
+CREATE TABLE employees (
+	employee_id uuid NOT NULL,
+	last_name varchar NOT NULL,
+	first_name varchar NOT NULL,
+	CONSTRAINT employee_pk PRIMARY KEY (employee_id)
+);
+
+INSERT INTO employees (employee_id, last_name, first_name) VALUES('407f5c96-0d83-4d37-badf-72b1d530f229'::uuid, 'Савицкая', 'Александра');
+INSERT INTO employees (employee_id, last_name, first_name) VALUES('35b6168e-c9e3-4744-8d30-f94d77c7d4ae'::uuid, 'Громов', 'Лев');
+INSERT INTO employees (employee_id, last_name, first_name) VALUES('bf4b2542-2e4b-49b3-b6fb-c001139c8da8'::uuid, 'Большаков', 'Марк');
+INSERT INTO employees (employee_id, last_name, first_name) VALUES('3c854726-3678-457c-9930-9465d3380627'::uuid, 'Ильин', 'Ярослав');
+INSERT INTO employees (employee_id, last_name, first_name) VALUES('68bddc84-4bda-4767-97e5-712188628a96'::uuid, 'Егорова', 'Ева');
+INSERT INTO employees (employee_id, last_name, first_name) VALUES('b118fa4e-55bd-4997-8566-a7dc1d4878dc'::uuid, 'Васильев', 'Максим');
+INSERT INTO employees (employee_id, last_name, first_name) VALUES('367e1888-059c-4b08-875f-f3000f731286'::uuid, 'Большаков', 'Тимофей');
+INSERT INTO employees (employee_id, last_name, first_name) VALUES('af956464-8f62-4c18-8eac-f7ee7e9a0245'::uuid, 'Королева', 'Елизавета');
+INSERT INTO employees (employee_id, last_name, first_name) VALUES('b5b7b38c-368c-4e8d-9f28-5ef0e2e76092'::uuid, 'Петухов', 'Даниил');
+INSERT INTO employees (employee_id, last_name, first_name) VALUES('7e75b76c-66d8-41f1-94bc-74434795ca6d'::uuid, 'Лобанов', 'Станислав');
+INSERT INTO employees (employee_id, last_name, first_name) VALUES('1609a4f0-18ab-4ca6-a6a7-7bffddda4d6f'::uuid, 'Николаев', 'Пётр');
+INSERT INTO employees (employee_id, last_name, first_name) VALUES('f223a860-88d1-4528-910f-86e74d92a278'::uuid, 'Никонов', 'Матвей');
+INSERT INTO employees (employee_id, last_name, first_name) VALUES('6d301794-e345-4c0a-87f6-796c5a494a65'::uuid, 'Смирнов', 'Максим');
+INSERT INTO employees (employee_id, last_name, first_name) VALUES('930a7f1e-4793-4a07-a7ad-0804d23810ac'::uuid, 'Бессонова', 'Анна');
+INSERT INTO employees (employee_id, last_name, first_name) VALUES('e714c73e-b554-40ab-9d48-88f781d6e79f'::uuid, 'Васильев', 'Артём');
+INSERT INTO employees (employee_id, last_name, first_name) VALUES('6b151812-ebf4-412f-8e64-29464eb5ef7a'::uuid, 'Смирнов', 'Георгий');
+INSERT INTO employees (employee_id, last_name, first_name) VALUES('7f5d5181-606e-4c58-a81d-7e848dec6242'::uuid, 'Николаева', 'Надежда');
+INSERT INTO employees (employee_id, last_name, first_name) VALUES('52368a32-a5da-4f42-8de0-dff038cf09c3'::uuid, 'Родионова', 'Мария');
+INSERT INTO employees (employee_id, last_name, first_name) VALUES('a93e9932-ffaa-46aa-8299-a24fff1563aa'::uuid, 'Андреев', 'Пётр');
+INSERT INTO employees (employee_id, last_name, first_name) VALUES('24841429-3b8f-4fcd-9db1-297c345101ed'::uuid, 'Лобанова', 'Ника');
+INSERT INTO employees (employee_id, last_name, first_name) VALUES('b6491c30-bab2-4154-8a59-962321645e20'::uuid, 'Лазарев', 'Даниил');
+INSERT INTO employees (employee_id, last_name, first_name) VALUES('c3566bc7-352b-4314-9551-ce1bd83bfdba'::uuid, 'Румянцева', 'Маргарита');
+INSERT INTO employees (employee_id, last_name, first_name) VALUES('3e3f468c-8909-49c2-8aed-79da3e3d7633'::uuid, 'Нечаева', 'Виктория');
+INSERT INTO employees (employee_id, last_name, first_name) VALUES('c8cec310-f059-40e1-8d84-ebdc32f356bf'::uuid, 'Черкасова', 'Марина');
+INSERT INTO employees (employee_id, last_name, first_name) VALUES('3260d397-3f17-436f-bc3d-353555581592'::uuid, 'Егоров', 'Михаил');
+INSERT INTO employees (employee_id, last_name, first_name) VALUES('da64749a-108a-4895-8637-44af6f648630'::uuid, 'Кольцова', 'Полина');
+INSERT INTO employees (employee_id, last_name, first_name) VALUES('cf90428a-47f4-4fab-8dbe-86b2684e5fa9'::uuid, 'Зуева', 'Есения');
+INSERT INTO employees (employee_id, last_name, first_name) VALUES('5850d97a-4bee-4d1c-a69a-34e8341d1a86'::uuid, 'Родионов', 'Денис');
+INSERT INTO employees (employee_id, last_name, first_name) VALUES('461eefe5-948a-471f-bd54-2e1742401798'::uuid, 'Белов', 'Максим');
+INSERT INTO employees (employee_id, last_name, first_name) VALUES('3913d64b-cfe6-468b-a1ca-50b1b6a4781d'::uuid, 'Григорьев', 'Глеб');
+INSERT INTO employees (employee_id, last_name, first_name) VALUES('f5ab1915-6650-444f-87c5-196bb9753b2a'::uuid, 'Борисова', 'Кристина');
+INSERT INTO employees (employee_id, last_name, first_name) VALUES('b491bcc3-4dfd-423e-90b2-668f27cf7c5e'::uuid, 'Ковалева', 'Виктория');
+INSERT INTO employees (employee_id, last_name, first_name) VALUES('d7524c85-4135-4946-ab80-034cb24b5ee5'::uuid, 'Вишневская', 'Анна');
+INSERT INTO employees (employee_id, last_name, first_name) VALUES('1e6351b7-b70e-446a-a86d-2828c8927137'::uuid, 'Карасев', 'Иван');
+INSERT INTO employees (employee_id, last_name, first_name) VALUES('bd816d39-aed7-4461-b4fe-ebee59350583'::uuid, 'Матвеев', 'Максим');
+INSERT INTO employees (employee_id, last_name, first_name) VALUES('0d57487c-64e7-4013-9edb-7d87ace0bd02'::uuid, 'Булгаков', 'Мирон');
+INSERT INTO employees (employee_id, last_name, first_name) VALUES('e39391c4-4981-45f8-8cd5-cf8085f50659'::uuid, 'Волков', 'Илья');
+INSERT INTO employees (employee_id, last_name, first_name) VALUES('6affb213-55f7-46ca-8cab-20142addaeee'::uuid, 'Баранова', 'Анастасия');
+INSERT INTO employees (employee_id, last_name, first_name) VALUES('a42b0bb9-ce46-45e9-83ba-de95da96c505'::uuid, 'Гусев', 'Роман');
+INSERT INTO employees (employee_id, last_name, first_name) VALUES('6b9f1327-0b3d-404a-bd67-ce2262f2d097'::uuid, 'Никонов', 'Даниил');
+INSERT INTO employees (employee_id, last_name, first_name) VALUES('5248fe8c-8821-424c-bddf-1e8601555392'::uuid, 'Алексеев', 'Арсений');
+INSERT INTO employees (employee_id, last_name, first_name) VALUES('36b387d8-b62a-41f3-83d3-c952c32a1553'::uuid, 'Осипова', 'Мария');
+INSERT INTO employees (employee_id, last_name, first_name) VALUES('bb55c500-da38-4e68-b869-3c9f19b72ceb'::uuid, 'Безруокв', 'Артем');
+INSERT INTO employees (employee_id, last_name, first_name) VALUES('edb5d23b-81de-4cfe-8511-e0e34c4e29e8'::uuid, 'Ткачев', 'Гордей');
+INSERT INTO employees (employee_id, last_name, first_name) VALUES('4111f324-6e76-4d81-8f3e-549afd01a80a'::uuid, 'Старостина', 'Екатерина');
+
+CREATE TABLE projects (
+	project_id uuid NOT NULL,
+	project_name varchar NOT NULL,
+	CONSTRAINT projects_pk PRIMARY KEY (project_id)
+);
+INSERT INTO projects
+(project_id, project_name)
+VALUES('56feb815-a8fe-4a19-a30b-9451b950baab'::uuid, 'Back office');
+INSERT INTO projects
+(project_id, project_name)
+VALUES('1937105a-fe8d-4cf1-b7ba-91e749f63363'::uuid, 'ГИС КАП');
+INSERT INTO projects
+(project_id, project_name)
+VALUES('e62d6fec-13af-4707-afe0-dffc9ce3eaba'::uuid, 'itobbaco');
+INSERT INTO projects
+(project_id, project_name)
+VALUES('dcde2895-501b-4b9a-a375-4fb6541fa5ec'::uuid, 'Цифровой акциз');
+
+CREATE TABLE titles (
+	title_id uuid NOT NULL,
+	title_name varchar NOT NULL,
+	CONSTRAINT titles_pk PRIMARY KEY (title_id)
+);
+
+INSERT INTO titles (title_id, title_name) VALUES('2d7feb0d-4dad-47d7-a095-6ee4a2129619'::uuid, 'CEO');
+INSERT INTO titles (title_id, title_name) VALUES('43aeecd8-4705-4216-900b-fd112756d5d9'::uuid, 'HR');
+INSERT INTO titles (title_id, title_name) VALUES('1231945e-6089-4372-8fe8-a4f7133c1984'::uuid, 'project manager');
+INSERT INTO titles (title_id, title_name) VALUES('b2d75c4d-40e2-477c-b06c-8f4667d81607'::uuid, 'backend developer');
+INSERT INTO titles (title_id, title_name) VALUES('4837cbb5-b35f-45b3-a5c3-e143ff041398'::uuid, 'frontend developer');
+INSERT INTO titles (title_id, title_name) VALUES('d132a4d1-c578-4d87-ab51-d6c9f1703cd5'::uuid, 'qa engineer');
+INSERT INTO titles (title_id, title_name) VALUES('96ace000-91a3-4c32-859a-4e0af136a68a'::uuid, 'system analyst');
+
+CREATE TABLE positions (
+	id uuid NOT NULL,
+	employee_id uuid NULL,
+	project_id uuid NULL,
+	title_id uuid NULL,
+	salary numeric(19, 2) NOT NULL,
+	CONSTRAINT positions_employee_id_fkey FOREIGN KEY (employee_id) REFERENCES employees(employee_id),
+	CONSTRAINT positions_project_id_fkey FOREIGN KEY (project_id) REFERENCES projects(project_id),
+	CONSTRAINT positions_title_id_fkey FOREIGN KEY (title_id) REFERENCES titles(title_id)
+);
+
+INSERT INTO positions (id, employee_id, project_id, title_id, salary) VALUES('4864563b-8e11-4b8f-a04a-524bab68975f'::uuid, '407f5c96-0d83-4d37-badf-72b1d530f229'::uuid, '56feb815-a8fe-4a19-a30b-9451b950baab'::uuid, '2d7feb0d-4dad-47d7-a095-6ee4a2129619'::uuid, 500000.00);
+INSERT INTO positions (id, employee_id, project_id, title_id, salary) VALUES('945b763e-8b6a-4e3a-a78e-f138c660c87f'::uuid, 'da64749a-108a-4895-8637-44af6f648630'::uuid, '56feb815-a8fe-4a19-a30b-9451b950baab'::uuid, '43aeecd8-4705-4216-900b-fd112756d5d9'::uuid, 150000.00);
+INSERT INTO positions (id, employee_id, project_id, title_id, salary) VALUES('96a09b6e-c1c2-480c-842d-d53cf7386bda'::uuid, 'cf90428a-47f4-4fab-8dbe-86b2684e5fa9'::uuid, '56feb815-a8fe-4a19-a30b-9451b950baab'::uuid, '43aeecd8-4705-4216-900b-fd112756d5d9'::uuid, 160000.00);
+INSERT INTO positions (id, employee_id, project_id, title_id, salary) VALUES('7b35f6bd-a930-4f42-b027-0b5e97979183'::uuid, '461eefe5-948a-471f-bd54-2e1742401798'::uuid, '56feb815-a8fe-4a19-a30b-9451b950baab'::uuid, '43aeecd8-4705-4216-900b-fd112756d5d9'::uuid, 175000.00);
+INSERT INTO positions (id, employee_id, project_id, title_id, salary) VALUES('9f97c10f-e2e2-440f-ab44-c2e30a276fe2'::uuid, '35b6168e-c9e3-4744-8d30-f94d77c7d4ae'::uuid, '1937105a-fe8d-4cf1-b7ba-91e749f63363'::uuid, '1231945e-6089-4372-8fe8-a4f7133c1984'::uuid, 225000.00);
+INSERT INTO positions (id, employee_id, project_id, title_id, salary) VALUES('d03826aa-2d7e-4c6b-a185-6a92eb29c36b'::uuid, 'bf4b2542-2e4b-49b3-b6fb-c001139c8da8'::uuid, '1937105a-fe8d-4cf1-b7ba-91e749f63363'::uuid, 'b2d75c4d-40e2-477c-b06c-8f4667d81607'::uuid, 180000.00);
+INSERT INTO positions (id, employee_id, project_id, title_id, salary) VALUES('22b04c56-0704-4480-9487-e1dc533b0b07'::uuid, '3c854726-3678-457c-9930-9465d3380627'::uuid, '1937105a-fe8d-4cf1-b7ba-91e749f63363'::uuid, 'b2d75c4d-40e2-477c-b06c-8f4667d81607'::uuid, 55000.00);
+INSERT INTO positions (id, employee_id, project_id, title_id, salary) VALUES('d3ce07f2-3b87-4e62-a9be-0f26f5b95d51'::uuid, '68bddc84-4bda-4767-97e5-712188628a96'::uuid, '1937105a-fe8d-4cf1-b7ba-91e749f63363'::uuid, '4837cbb5-b35f-45b3-a5c3-e143ff041398'::uuid, 135000.00);
+INSERT INTO positions (id, employee_id, project_id, title_id, salary) VALUES('4e48dfd3-2908-44c7-ac56-426e97e07dcc'::uuid, 'b118fa4e-55bd-4997-8566-a7dc1d4878dc'::uuid, '1937105a-fe8d-4cf1-b7ba-91e749f63363'::uuid, '96ace000-91a3-4c32-859a-4e0af136a68a'::uuid, 80000.00);
+INSERT INTO positions (id, employee_id, project_id, title_id, salary) VALUES('938691a8-bebb-4b69-8c20-5adb32a245bb'::uuid, '367e1888-059c-4b08-875f-f3000f731286'::uuid, '1937105a-fe8d-4cf1-b7ba-91e749f63363'::uuid, '96ace000-91a3-4c32-859a-4e0af136a68a'::uuid, 100000.00);
+INSERT INTO positions (id, employee_id, project_id, title_id, salary) VALUES('f559001f-5ac4-428e-838f-58245665319f'::uuid, 'af956464-8f62-4c18-8eac-f7ee7e9a0245'::uuid, '1937105a-fe8d-4cf1-b7ba-91e749f63363'::uuid, '96ace000-91a3-4c32-859a-4e0af136a68a'::uuid, 243000.00);
+INSERT INTO positions (id, employee_id, project_id, title_id, salary) VALUES('fe069a13-1e5d-4af3-a58a-560f8f0a0bde'::uuid, '5248fe8c-8821-424c-bddf-1e8601555392'::uuid, '1937105a-fe8d-4cf1-b7ba-91e749f63363'::uuid, 'd132a4d1-c578-4d87-ab51-d6c9f1703cd5'::uuid, 75000.00);
+INSERT INTO positions (id, employee_id, project_id, title_id, salary) VALUES('259d2657-70cf-4a1f-8f46-72a25564a45a'::uuid, '36b387d8-b62a-41f3-83d3-c952c32a1553'::uuid, '1937105a-fe8d-4cf1-b7ba-91e749f63363'::uuid, 'd132a4d1-c578-4d87-ab51-d6c9f1703cd5'::uuid, 125000.00);
+INSERT INTO positions (id, employee_id, project_id, title_id, salary) VALUES('3d1616db-632b-4813-87ea-4b39a08030e1'::uuid, 'bb55c500-da38-4e68-b869-3c9f19b72ceb'::uuid, '1937105a-fe8d-4cf1-b7ba-91e749f63363'::uuid, 'd132a4d1-c578-4d87-ab51-d6c9f1703cd5'::uuid, 200000.00);
+INSERT INTO positions (id, employee_id, project_id, title_id, salary) VALUES('0559ba6a-56c5-44a3-8403-6b0e35cfc0a3'::uuid, 'edb5d23b-81de-4cfe-8511-e0e34c4e29e8'::uuid, '1937105a-fe8d-4cf1-b7ba-91e749f63363'::uuid, 'd132a4d1-c578-4d87-ab51-d6c9f1703cd5'::uuid, 146500.00);
+INSERT INTO positions (id, employee_id, project_id, title_id, salary) VALUES('7b0f4efb-7e38-40bc-97f1-a4331bbdb2a5'::uuid, '4111f324-6e76-4d81-8f3e-549afd01a80a'::uuid, '1937105a-fe8d-4cf1-b7ba-91e749f63363'::uuid, 'd132a4d1-c578-4d87-ab51-d6c9f1703cd5'::uuid, 112000.00);
+INSERT INTO positions (id, employee_id, project_id, title_id, salary) VALUES('15eb727d-9427-40a5-a8ef-5f862ad3561d'::uuid, 'b5b7b38c-368c-4e8d-9f28-5ef0e2e76092'::uuid, 'e62d6fec-13af-4707-afe0-dffc9ce3eaba'::uuid, '1231945e-6089-4372-8fe8-a4f7133c1984'::uuid, 300000.00);
+INSERT INTO positions (id, employee_id, project_id, title_id, salary) VALUES('a1f645eb-9d68-4b5a-a6a8-17cad97c0601'::uuid, '7e75b76c-66d8-41f1-94bc-74434795ca6d'::uuid, 'e62d6fec-13af-4707-afe0-dffc9ce3eaba'::uuid, 'b2d75c4d-40e2-477c-b06c-8f4667d81607'::uuid, 95000.00);
+INSERT INTO positions (id, employee_id, project_id, title_id, salary) VALUES('20df7a25-58df-4e30-be30-9f101a42a46a'::uuid, '1609a4f0-18ab-4ca6-a6a7-7bffddda4d6f'::uuid, 'e62d6fec-13af-4707-afe0-dffc9ce3eaba'::uuid, '4837cbb5-b35f-45b3-a5c3-e143ff041398'::uuid, 50000.00);
+INSERT INTO positions (id, employee_id, project_id, title_id, salary) VALUES('fde03138-c459-4f9b-9869-7a182d250b04'::uuid, 'f223a860-88d1-4528-910f-86e74d92a278'::uuid, 'e62d6fec-13af-4707-afe0-dffc9ce3eaba'::uuid, '4837cbb5-b35f-45b3-a5c3-e143ff041398'::uuid, 135000.00);
+INSERT INTO positions (id, employee_id, project_id, title_id, salary) VALUES('657b8d50-b5cc-4009-b67c-9fdb78738ecc'::uuid, '6d301794-e345-4c0a-87f6-796c5a494a65'::uuid, 'e62d6fec-13af-4707-afe0-dffc9ce3eaba'::uuid, '4837cbb5-b35f-45b3-a5c3-e143ff041398'::uuid, 150000.00);
+INSERT INTO positions (id, employee_id, project_id, title_id, salary) VALUES('56baff6d-e6a6-402a-af79-23aa237ab10e'::uuid, '930a7f1e-4793-4a07-a7ad-0804d23810ac'::uuid, 'e62d6fec-13af-4707-afe0-dffc9ce3eaba'::uuid, '96ace000-91a3-4c32-859a-4e0af136a68a'::uuid, 250000.00);
+INSERT INTO positions (id, employee_id, project_id, title_id, salary) VALUES('010a2656-0c2e-426b-ad80-5245762654cb'::uuid, 'e714c73e-b554-40ab-9d48-88f781d6e79f'::uuid, 'e62d6fec-13af-4707-afe0-dffc9ce3eaba'::uuid, 'd132a4d1-c578-4d87-ab51-d6c9f1703cd5'::uuid, 220000.00);
+INSERT INTO positions (id, employee_id, project_id, title_id, salary) VALUES('247f62cb-57d7-4ed4-af4f-8cd23209e4eb'::uuid, '6b151812-ebf4-412f-8e64-29464eb5ef7a'::uuid, 'e62d6fec-13af-4707-afe0-dffc9ce3eaba'::uuid, 'd132a4d1-c578-4d87-ab51-d6c9f1703cd5'::uuid, 135000.00);
+INSERT INTO positions (id, employee_id, project_id, title_id, salary) VALUES('e2ececfb-57e8-4949-97ad-922bc2cb796e'::uuid, 'f5ab1915-6650-444f-87c5-196bb9753b2a'::uuid, 'e62d6fec-13af-4707-afe0-dffc9ce3eaba'::uuid, 'd132a4d1-c578-4d87-ab51-d6c9f1703cd5'::uuid, 95000.00);
+INSERT INTO positions (id, employee_id, project_id, title_id, salary) VALUES('4edb90d8-e6df-4e5d-a859-5a4804a91648'::uuid, '7f5d5181-606e-4c58-a81d-7e848dec6242'::uuid, 'dcde2895-501b-4b9a-a375-4fb6541fa5ec'::uuid, 'b2d75c4d-40e2-477c-b06c-8f4667d81607'::uuid, 100000.00);
+INSERT INTO positions (id, employee_id, project_id, title_id, salary) VALUES('d48c14b8-bf62-4211-b6ee-33fa4f700c94'::uuid, '24841429-3b8f-4fcd-9db1-297c345101ed'::uuid, 'dcde2895-501b-4b9a-a375-4fb6541fa5ec'::uuid, 'b2d75c4d-40e2-477c-b06c-8f4667d81607'::uuid, 345000.00);
+INSERT INTO positions (id, employee_id, project_id, title_id, salary) VALUES('a45dd89b-3a7d-4fe5-b2d0-078451a02a89'::uuid, 'b6491c30-bab2-4154-8a59-962321645e20'::uuid, 'dcde2895-501b-4b9a-a375-4fb6541fa5ec'::uuid, '96ace000-91a3-4c32-859a-4e0af136a68a'::uuid, 147000.00);
+INSERT INTO positions (id, employee_id, project_id, title_id, salary) VALUES('f7dff6dc-bf17-4bed-b1d1-08776998f316'::uuid, 'c3566bc7-352b-4314-9551-ce1bd83bfdba'::uuid, 'dcde2895-501b-4b9a-a375-4fb6541fa5ec'::uuid, '96ace000-91a3-4c32-859a-4e0af136a68a'::uuid, 95500.00);
+INSERT INTO positions (id, employee_id, project_id, title_id, salary) VALUES('af70e76b-1e88-4fc6-8c30-9a55cb1aee67'::uuid, '3e3f468c-8909-49c2-8aed-79da3e3d7633'::uuid, 'dcde2895-501b-4b9a-a375-4fb6541fa5ec'::uuid, '96ace000-91a3-4c32-859a-4e0af136a68a'::uuid, 320000.00);
+INSERT INTO positions (id, employee_id, project_id, title_id, salary) VALUES('78732fb8-a201-4371-9705-cf247e2b0e18'::uuid, 'c8cec310-f059-40e1-8d84-ebdc32f356bf'::uuid, 'dcde2895-501b-4b9a-a375-4fb6541fa5ec'::uuid, 'd132a4d1-c578-4d87-ab51-d6c9f1703cd5'::uuid, 500000.00);
+INSERT INTO positions (id, employee_id, project_id, title_id, salary) VALUES('a804b283-d01e-4fb4-be2c-6da380190cfa'::uuid, 'b5b7b38c-368c-4e8d-9f28-5ef0e2e76092'::uuid, 'dcde2895-501b-4b9a-a375-4fb6541fa5ec'::uuid, '1231945e-6089-4372-8fe8-a4f7133c1984'::uuid, 156000.00);
+INSERT INTO positions (id, employee_id, project_id, title_id, salary) VALUES('c91a6546-cb4d-4b7f-86c0-4d0dbf80b487'::uuid, '3260d397-3f17-436f-bc3d-353555581592'::uuid, 'dcde2895-501b-4b9a-a375-4fb6541fa5ec'::uuid, 'd132a4d1-c578-4d87-ab51-d6c9f1703cd5'::uuid, 189000.00);
+INSERT INTO positions (id, employee_id, project_id, title_id, salary) VALUES('e86b2091-e219-4b4f-a108-f724869a0277'::uuid, 'f5ab1915-6650-444f-87c5-196bb9753b2a'::uuid, 'dcde2895-501b-4b9a-a375-4fb6541fa5ec'::uuid, 'd132a4d1-c578-4d87-ab51-d6c9f1703cd5'::uuid, 138000.00);
